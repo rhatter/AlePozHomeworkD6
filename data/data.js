9e3dcb30bfdf0e6f0287a5d083fbadf78bb56f13
rhatter <49161216+rhatter@users.noreply.github.com>
2023-05-19 15:23:31 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,79 @@
+let data = {
+    imgDir :        ['https://i.pinimg.com/236x/46/ff/a9/46ffa96e6aec419f5accdcf80d7e90f4.jpg'
+    ,'https://i.pinimg.com/236x/0d/be/c6/0dbec6733a8e0e7e707fdaf72ca73401.jpg'
+    ,'https://i.pinimg.com/236x/c7/82/27/c782274a0e49fe58df4a9faac528d6a0.jpg'
+    ,'https://i.pinimg.com/236x/9e/48/47/9e48471025c1a967a7db534e929efa9c.jpg'
+    ,'https://i.pinimg.com/236x/c7/67/b2/c767b25578d752877f4b1a1c00bf5b41.jpg'
+    ,'https://i.pinimg.com/236x/d2/56/5a/d2565a13929dd34b64b08febae17b97d.jpg'
+    ,'https://i.pinimg.com/236x/1f/bd/c0/1fbdc099104688c2d30d95d15fa6eac8.jpg'
+    ,'https://i.pinimg.com/236x/8f/db/c7/8fdbc795209685fa5f83711f7281a49c.jpg'
+    ,'https://i.pinimg.com/236x/b2/03/3b/b2033b91258f24caf3b21c84ade34995.jpg'
+    ,'https://i.pinimg.com/236x/c7/60/b5/c760b542852fe915abedc4ce25e4a836.jpg'
+    ,'no'],
+    imgAlt :        ['Profumo chanel'
+    ,'Byredo perfume'
+    ,'Hotel Lobby Candle'
+    ,'Maarten Leon'
+    ,'Polar Bear'
+    ,'Vintage Trendy Bar Cart Poster / Print'
+    ,'Pass the wine'
+    ,'Wine brand Stompy wants you to “sip more, snob less”'
+    ,'Avocados'
+    ,'Make yourself proud'
+    ,'no'],
+    bachDesc:       ['Bacheca personale'
+    ,'Profilo'
+    ,'Profilo'
+    ,'Bacheca personale'
+    ,'Graphic'
+    ,'Graphic'
+    ,'Photograpic'
+    ,'Design'
+    ,'Photo'
+    ,'Dress'
+    ,'no'],
+    linkTx :        ['dnewton.com'
+    ,'atcosmetic.hu'
+    ,'hotelhobbycandle.com'
+    ,'desenio.com'
+    ,'casualpolarbear.com'
+    ,'vintagetrendy.com'
+    ,'spreadsparrow.com'
+    ,'creativereview.co.uk'
+    ,'foodposing.com'
+    ,'niceshirt.com'
+    ,'no'],
+    titleCard:      ['Perfume - David Newton'
+    ,'Byredi Skincare'
+    ,'Hotel Lobby Candle - Island'
+    ,'Pagina del Pin Maarten Léon - your best friends forget you get old'
+    ,'Searching - Casual Polar Bear'
+    ,'Vintage Trendy Bar Cart Poster / Print'
+    ,'Pass the wine'
+    ,'Wine brand Stompy wants you to “sip more, snob less”'
+    ,'Fresh avocados'
+    ,'Make green yoursel'
+    ,'no'],
+    profileImgDir:  [null
+    ,'https://i.pinimg.com/75x75_RS/ee/19/fe/ee19fe5351452d2e6219d8466d649e78.jpg'
+    ,'https://i.pinimg.com/75x75_RS/cc/ba/cc/ccbacc160b701e1953dec47a792728d6.jpg'
+    ,null
+    ,null
+    ,'https://i.pinimg.com/75x75_RS/9a/0f/45/9a0f455573bb24a919926e955893072b.jpg'
+    ,'https://i.pinimg.com/75x75_RS/04/32/3f/04323fa48b416fd44bcc1fa94788d658.jpg'
+    ,null
+    ,null
+    ,'https://i.pinimg.com/75x75_RS/d1/22/98/d12298336a8f9ffd0ef80a2778d11214.jpg'
+    ,'no'],
+    profText:       [null
+    ,'A&T Cosmetics I Product development services for Beauty Brands'
+    ,'Hotel Lobby Candle'
+    ,null
+    ,null
+    ,'carol'
+    ,'Spade & Sparrows  Wine'
+    ,'Wine brand Stompy wants you to “sip more, snob less”'
+    ,null
+    ,'Eleonora'
+    ,'no']
+}
